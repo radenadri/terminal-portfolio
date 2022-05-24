3ff2aaba1d12cd77f54c1b0c46e263bfbcfa2023
Adriana Eka Prayudha <radenadriep@gmail.com>
2022-05-24 15:12:29 +0700

    Add notice of typing gui version on load'

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -163,6 +163,7 @@ export const banner = (args?: string[]): string => {
 
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
+Type 'gui' to open the GUI version of portolio.
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
 `;
 };
